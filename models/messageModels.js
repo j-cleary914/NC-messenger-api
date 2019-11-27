b3857110762be9749740c56f48f774f836e64a86
Matt Daley <mdaleyhk@gmail.com>
2019-11-27 13:04:27 +0000

    Add createMessage in models for liasing with db

---
+++ models/messageModels.js
@@ -0,0 +1,9 @@
+const connection = require("../db/connection");
+
+exports.createMessage = data => {
+  const { sender, message } = data;
+
+  return connection("messages")
+    .insert({ sender, message })
+    .returning("*");
+};
