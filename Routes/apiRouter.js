b81de4e6840cc5c6d68a4d6c2f1a83d756bb06fe
Jasmine Chatfield <jazzchatfield@gmail.com>
2019-11-27 14:32:50 +0000

    update apiRouter for usersRouter

--- Routes/apiRouter.js
+++ Routes/apiRouter.js
@@ -1,6 +1,9 @@
 const apiRouter = require("express").Router();
 const { sendEndpoints } = require("../controllers/apiController");
+const { usersRouter } = require("./usersRouter");
 
 apiRouter.route("/").get(sendEndpoints);
+
+apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;
